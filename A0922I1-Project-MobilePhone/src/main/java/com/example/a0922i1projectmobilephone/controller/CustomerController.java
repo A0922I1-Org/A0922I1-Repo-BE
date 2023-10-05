f20e1a322b7faabdec8f3c91a13db711b8308667
Module4 <ngocduy40k02@gmail.com>
2023-10-06 06:58:06 +0700

    update

--- A0922I1-Project-MobilePhone/src/main/java/com/example/a0922i1projectmobilephone/controller/CustomerController.java
+++ A0922I1-Project-MobilePhone/src/main/java/com/example/a0922i1projectmobilephone/controller/CustomerController.java
@@ -5,13 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CustomerController {
    @Autowired
     private ICustomerService customerService;
