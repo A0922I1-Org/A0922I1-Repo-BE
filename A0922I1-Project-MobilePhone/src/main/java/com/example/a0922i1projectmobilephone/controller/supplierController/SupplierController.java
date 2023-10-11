feabaa646ca8cd359d3511db781ab54a5d3efe05
hnghia1902 <102527569+hnghia1902@users.noreply.github.com>
2023-10-11 18:41:42 +0700

    update

--- A0922I1-Project-MobilePhone/src/main/java/com/example/a0922i1projectmobilephone/controller/supplierController/SupplierController.java
+++ A0922I1-Project-MobilePhone/src/main/java/com/example/a0922i1projectmobilephone/controller/supplierController/SupplierController.java
@@ -7,12 +7,11 @@ import com.example.a0922i1projectmobilephone.service.supplierService.update.IUpd
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/supplier")
 @CrossOrigin("*")
 public class SupplierController {
